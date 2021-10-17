a9ea4c356546483b4eff816d3ab6b9186f129471
sousa47 <davimfs7@gmail.com>
2021-10-17 21:33:45 +0100

    Weapon Chooser

--- Assets/Player/GameController.cs
+++ Assets/Player/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,6 +15,7 @@ public class GameController : MonoBehaviour
     public Image heartImage5;
 
     public Image halfHeart;
+    public Image empty;
 
     public int score = 0;
     public int life = 100;
@@ -26,6 +28,14 @@ public class GameController : MonoBehaviour
 
     public GameObject canvasPause;
     public Animator canvasPauseAnimator;
+
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject buttonGun1;
+    public GameObject buttonGun2;
+    public GameObject gun1;
+    public GameObject gun2;
 
 
     // Start is called before the first frame update
@@ -73,59 +83,59 @@ public class GameController : MonoBehaviour
         if(life < 0) 
         {
             //die;
-            heartImage1.sprite  = null;
-            heartImage2.sprite  = null;
-            heartImage3.sprite  = null;
-            heartImage4.sprite  = null;
-            heartImage5.sprite  = null;
+            heartImage1.sprite  = empty.sprite;
+            heartImage2.sprite  = empty.sprite;
+            heartImage3.sprite  = empty.sprite;
+            heartImage4.sprite  = empty.sprite;
+            heartImage5.sprite  = empty.sprite;
         } 
         else if(life < 10) 
         {
             heartImage1.sprite  = halfHeart.sprite;
-            heartImage2.sprite  = null;
-            heartImage3.sprite  = null;
-            heartImage4.sprite  = null;
-            heartImage5.sprite  = null;
+            heartImage2.sprite  = empty.sprite;
+            heartImage3.sprite  = empty.sprite;
+            heartImage4.sprite  = empty.sprite;
+            heartImage5.sprite  = empty.sprite;
         } 
         else if(life < 20) 
         {
-            heartImage2.sprite  = null;
-            heartImage3.sprite  = null;
-            heartImage4.sprite  = null;
-            heartImage5.sprite  = null;
+            heartImage2.sprite  = empty.sprite;
+            heartImage3.sprite  = empty.sprite;
+            heartImage4.sprite  = empty.sprite;
+            heartImage5.sprite  = empty.sprite;
         } 
         else if(life < 30) 
         {
             heartImage2.sprite  = halfHeart.sprite ;
-            heartImage3.sprite  = null;
-            heartImage4.sprite  = null;
-            heartImage5.sprite  = null;
+            heartImage3.sprite  = empty.sprite;
+            heartImage4.sprite  = empty.sprite;
+            heartImage5.sprite  = empty.sprite;
         } 
         else if(life < 40) 
         {
-            heartImage3.sprite  = null;
-            heartImage4.sprite  = null;
-            heartImage5.sprite  = null;
+            heartImage3.sprite  = empty.sprite;
+            heartImage4.sprite  = empty.sprite;
+            heartImage5.sprite  = empty.sprite;
         } 
         else if(life < 50) 
         {
             heartImage3.sprite  = halfHeart.sprite ;
-            heartImage4.sprite  = null;
-            heartImage5.sprite  = null;
+            heartImage4.sprite  = empty.sprite;
+            heartImage5.sprite  = empty.sprite;
         } 
         else if(life < 60) 
         {
-            heartImage4.sprite  = null;
-            heartImage5.sprite  = null;
+            heartImage4.sprite  = empty.sprite;
+            heartImage5.sprite  = empty.sprite;
         } 
         else if(life < 70) 
         {
             heartImage4.sprite  = halfHeart.sprite ;
-            heartImage5.sprite  = null;
+            heartImage5.sprite  = empty.sprite;
         } 
         else if(life < 80) 
         {
-            heartImage5.sprite  = null;
+            heartImage5.sprite  = empty.sprite;
         } 
         else if(life < 90) 
         {
@@ -135,6 +145,8 @@ public class GameController : MonoBehaviour
 
     public void Play() 
     {
+        buttonGun1.SetActive(false);
+        buttonGun2.SetActive(false);
         Time.timeScale = 1;
         paused = false;
         canvasPause.SetActive(paused);
@@ -142,10 +154,38 @@ public class GameController : MonoBehaviour
     }
     public void Settings() 
     {
-
+        Debug.Log("sss");
+        button1.SetActive(false);
+        button2.SetActive(false);
+        button3.SetActive(false);
+        buttonGun1.SetActive(true);
+        buttonGun2.SetActive(true);
+        //Time.timeScale = 0;
     }
     public void Exit() 
     {
         Application.Quit();
+    }
+    public void Gun1() 
+    {
+        playerAnimator.SetInteger("gun", 1);
+        gun1.SetActive(true);
+        gun2.SetActive(false);
+        button1.SetActive(true);
+        button2.SetActive(true);
+        button3.SetActive(true);
+        buttonGun1.SetActive(false);
+        buttonGun2.SetActive(false);
+    }
+    public void Gun2() 
+    {
+        playerAnimator.SetInteger("gun", 2);
+        gun1.SetActive(false);
+        gun2.SetActive(true);
+        button1.SetActive(true);
+        button2.SetActive(true);
+        button3.SetActive(true);
+        buttonGun1.SetActive(false);
+        buttonGun2.SetActive(false);
     }
 }
